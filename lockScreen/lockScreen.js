1982c1a62d5e8dcf74e2404d31f8efe106c95eba
carbonicality <215186372+carbonicality@users.noreply.github.com>
2026-09-16 20:02:32 +0100

    commit: lockScreen func, desktop prep, bugfix

--- lockScreen/lockScreen.js
+++ lockScreen/lockScreen.js
@@ -14,3 +14,36 @@ function updateDateTime() {
 
 updateDateTime();
 setInterval(updateDateTime, 1000);
+
+//temporary authentication functionality
+const PASSCODE='123456';
+const lockScreen=document.getElementById('lockScreen');
+const passcodeInput=document.getElementById('passcode');
+
+function switchState() {
+    window.location.href='../desktop/desktop.html';
+}
+
+function unlock() {
+    passcodeInput.disabled=true;
+    lockScreen.addEventListener('transitionend',(e)=>{
+        if (e.propertyName==='transform') switchState();
+    });
+    lockScreen.classList.add('unlocked');
+}
+
+function wrongPass() {
+    passcodeInput.classList.remove('shake');
+    void passcodeInput.offsetWidth;
+    passcodeInput.classList.add('shake');
+    setTimeout(()=>{passcodeInput.value=''},400);
+}
+
+function checkPasscode() {
+    if (passcodeInput.value===PASSCODE) unlock();
+    else wrongPass();
+}
+
+passcodeInput.addEventListener('keydown',(e)=>{
+    if (e.key==='Enter') checkPasscode();
+});
