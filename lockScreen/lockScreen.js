3e2297ef05b4ee2264f8391477a0c5873762f32a
daschdrioooo <alikambalosman8@gmail.com>
2026-09-16 17:44:41 +0000

    created: lock screen

--- lockScreen/lockScreen.js
+++ lockScreen/lockScreen.js
@@ -1,1 +1,16 @@
 lucide.createIcons();
+
+function updateDateTime() {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    document.getElementById('time').textContent = `${hours}:${minutes}`;
+
+    const options = { weekday: 'short', month: 'short', day: 'numeric' };
+    const dateStr = now.toLocaleDateString('en-GB', options).replace(',', '');
+    document.getElementById('date').textContent = dateStr.replace(',', '');
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000);
